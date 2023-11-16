5ee3b07577da571ed42ffa91fc741b0e5e6c35ee
이지연 <peaceprimavera@gmail.com>
2023-11-16 10:35:58 +0900

    오류 수정

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+
 import db from '@/db';
 import { RowDataPacket } from 'mysql2/promise';
 import Link from 'next/link';
@@ -22,15 +22,15 @@ export default async function Edit(props : editProps) {
     // 'update suho.board set 필드 = 변경값, 필드 = 변경값 where id = 변경할 아이디'
     // ('update suho.board set title = ?, content = ? where id = ?'), [title, content, id]
 
-    const [formData, setFormData] = useState<editPost>({
-        title : '',
-        content : ''
-    });
+    // const [formData, setFormData] = useState<editPost>({
+    //     title : '',
+    //     content : ''
+    // });
 
-    const changeEvent = (e : React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setFormData({...formData, [e.target.name] : e.target.value});
-        console.log(formData);
-    };
+    // const changeEvent = (e : React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    //     setFormData({...formData, [e.target.name] : e.target.value});
+    //     console.log(formData);
+    // };
     const submitEvent = async (e : React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
@@ -39,7 +39,7 @@ export default async function Edit(props : editProps) {
                 headers : {
                     'Content-Type' : 'application/json'
                 },
-                body : JSON.stringify(formData)
+                body : JSON.stringify(results)
             });
             if (res.ok) {
                 const data = await res.json();
@@ -63,9 +63,9 @@ export default async function Edit(props : editProps) {
             <div className="lg:max-w-5xl md:max-w-3xl max-w-sm mx-auto">
                 <div className="bg-white shadow-md mt-40">
                     <form method="post" onSubmit={submitEvent} className="p-[2%]">
-                        <p className="text-sm font-bold">닉네임</p><input type="text" name="name" defaultValue={formData.title} onChange={changeEvent} className="shadow text-gray-700 text-sm mb-2 border block p-1.5" /><br />
-                        <p className="text-sm font-bold">제목</p><input type="text" name="title" defaultValue={formData.title} onChange={changeEvent} className="shadow text-gray-700 text-sm mb-2 border block p-1.5"/><br />
-                        <p className="text-sm font-bold">내용</p><textarea name="content" defaultValue={formData.content} onChange={changeEvent} className="shadow text-gray-700 text-sm mb-2 border block p-1.5 w-full h-36"></textarea>
+                        <p className="text-sm font-bold">닉네임</p><input type="text" name="name" defaultValue={results[0].name} className="shadow text-gray-700 text-sm mb-2 border block p-1.5" /><br />
+                        <p className="text-sm font-bold">제목</p><input type="text" name="title" defaultValue={results[0].title} className="shadow text-gray-700 text-sm mb-2 border block p-1.5"/><br />
+                        <p className="text-sm font-bold">내용</p><textarea name="content" defaultValue={results[0].content} className="shadow text-gray-700 text-sm mb-2 border block p-1.5 w-full h-36"></textarea>
                         <div className="text-right my-5">
                             <button className="bg-blue-500 text-white px-4 py-2 rounded shadow-md hover:bg-blue-600 focus:outline-none mr-5">등록</button>
                             <button className="bg-red-500 text-white px-4 py-2 rounded shadow-md hover:bg-red-600 focus:outline-none"><Link href='/' className='text-white'>취소</Link></button>
